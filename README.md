0babb327f91c6b1190906e384580b72497d39e7f
kyler.little <kyler.little@wsu.edu>
2021-04-28 12:35:58 -0700

    restructure repo, add invite module, update ignore

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # the-movement-slackbots
+
+## Set Up
+
+Create venv
+```
+python3 -m venv venv
+```
+
+Activate
+```
+source venv/bin/activate
+```
+
+Install requirements
+```
+pip install requirements.txt
+```
+
+## Run Code
+
+```
+python invite
+```
---
+++ invite/__main__.py
@@ -0,0 +1,15 @@
+def main():
+    # 1. One piece of code, can be separate from everything
+    # Read in files from membership sign up
+
+    # Invite NEW users to Slack workspace
+    print('hello world')
+
+    # Read in the four files of people who signed up (download those from webflow)
+    # for each person in persons:
+    #   if I haven't invited this person yet: invite them()
+    #   have I invited them? Looking at a file.
+    #   after inviting them, append to a file
+
+if __name__ == '__main__':
+    main()
